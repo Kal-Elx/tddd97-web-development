4671260c18c4bbdccc6749b734c255fdc7baf11a
Kalle Johansson <kallej1johansson@gmail.com>
2021-01-26 18:12:01 +0100

    lab 1 step 4: sign-up mechanism

--- client/client.js
+++ client/client.js
@@ -18,7 +18,11 @@ function attachHandlers() {
 }
 
 function login(formData) {
-    // TODO: Login.
+    handle_login(formData.login_email.value, formData.login_password.value);
+}
+
+function handle_login(email, password) {
+    console.log(email, password);
     document.getElementById("login_form").reset();
 }
 
@@ -29,8 +33,31 @@ function signup(formData) {
         repeatPasswordInput.setCustomValidity("Passwords does not match.");
     } else {
         repeatPasswordInput.setCustomValidity("");
-        // TODO: Sign up.
-        document.getElementById("signup_form").reset();
+        let res = serverstub.signUp({
+            email: formData.signup_email.value,
+            password: formData.signup_password.value,
+            firstname: formData.first_name.value,
+            familyname: formData.family_name.value,
+            gender: formData.gender.value,
+            city: formData.city.value,
+            country: formData.country.value,
+        });
+        if (res.success) {
+            handle_login(formData.signup_email.value, formData.signup_password.value);
+            document.getElementById("signup_form").reset();
+        } else {
+            communicateToUser(res.message);
+        }
     }
     repeatPasswordInput.reportValidity();
+}
+
+function communicateToUser(msg) {
+    let messageBoxText = document.getElementById("message_box_text");
+    let messageBox = document.getElementById("message_box");
+    messageBoxText.innerText = msg;
+
+    // Animation from: https://codepen.io/mikeCky/pen/WWjLEq
+    messageBox.classList.remove("hidden_animation");
+    setTimeout(function () { messageBox.classList.add("hidden_animation"); }, 3000);
 }
