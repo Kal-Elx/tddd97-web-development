eaa8f949d068956f1b187c9c3e0d9e3b743d0edc
Kalle Johansson <kallej1johansson@gmail.com>
2021-01-26 17:04:02 +0100

    lab 1 step 3: user input validation

--- client/client.js
+++ client/client.js
@@ -1,8 +1,36 @@
-function displayView(signedIn) {
-    view_id = signedIn ? "profile_view" : "welcome_view";
-    document.getElementById("viewport").innerHTML = document.getElementById(view_id).innerHTML;
-};
-
 window.onload = function () {
     displayView(false);
+    attachHandlers();
 };
+
+function displayView(signedIn) {
+    let view = signedIn ? "profile_view" : "welcome_view";
+    document.getElementById("viewport").innerHTML = document.getElementById(view).innerHTML;
+};
+
+function attachHandlers() {
+    document.getElementById("login_form")?.setAttribute("onsubmit", "login(this); return false;");
+    document.getElementById("signup_form")?.setAttribute("onsubmit", "signup(this); return false;");
+    let repeatPasswordInput = document.getElementById("repeat_signup_password");
+    if (repeatPasswordInput != null) {
+        repeatPasswordInput.oninput = function () { repeatPasswordInput.setCustomValidity(""); };
+    }
+}
+
+function login(formData) {
+    // TODO: Login.
+    document.getElementById("login_form").reset();
+}
+
+function signup(formData) {
+    let repeatPasswordInput = document.getElementById("repeat_signup_password");
+
+    if (formData.signup_password.value != formData.repeat_signup_password.value) {
+        repeatPasswordInput.setCustomValidity("Passwords does not match.");
+    } else {
+        repeatPasswordInput.setCustomValidity("");
+        // TODO: Sign up.
+        document.getElementById("signup_form").reset();
+    }
+    repeatPasswordInput.reportValidity();
+}
