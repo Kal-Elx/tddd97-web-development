71e5a6dabcfc72b898b2a1769b2f784259d5aa91
Kalle Johansson <kallej1johansson@gmail.com>
2021-01-30 17:27:21 +0100

    lab 1 step 7: account tab

--- client/client.js
+++ client/client.js
@@ -1,7 +1,11 @@
 window.onload = function () {
-    let signedIn = localStorage.getItem("token") != null;
+    let signedIn = getToken() != null;
     displayView(signedIn);
 };
+
+function getToken() {
+    return localStorage.getItem("token");
+}
 
 function displayView(signedIn) {
     if (signedIn) {
@@ -18,6 +22,10 @@ function displayView(signedIn) {
             let panel_id = tab.getAttribute("data-panel");
             document.getElementById(panel_id).hidden = false;
         });
+        document.getElementById("sign_out_button").onclick = signOut;
+        document.getElementById("change_password_form")?.setAttribute("onsubmit", "changePassword(this); return false;");
+        let repeatPasswordInput = document.getElementById("repeat_change_password_input");
+        repeatPasswordInput.oninput = function () { repeatPasswordInput.setCustomValidity(""); };
 
     } else {
         document.getElementById("viewport").innerHTML = document.getElementById("welcome_view").innerHTML;
@@ -25,7 +33,8 @@ function displayView(signedIn) {
         // Attach handlers.
         document.getElementById("login_form")?.setAttribute("onsubmit", "login(this); return false;");
         document.getElementById("signup_form")?.setAttribute("onsubmit", "signup(this); return false;");
-        document.getElementById("repeat_signup_password").oninput = function () { repeatPasswordInput.setCustomValidity(""); };
+        let repeatPasswordInput = document.getElementById("repeat_signup_password");
+        repeatPasswordInput.oninput = function () { repeatPasswordInput.setCustomValidity(""); };
     }
 }
 
@@ -35,22 +44,21 @@ function login(formData) {
 
 function handle_login(email, password) {
     let res = serverstub.signIn(email, password);
+
     if (res.success) {
         let token = res.data;
         localStorage.setItem("token", token);
         document.getElementById("login_form").reset();
         displayView(true);
     } else {
-        communicateToUser(res.message);
+        communicateToUser(res.message, "welcome_view");
     }
 }
 
 function signup(formData) {
     let repeatPasswordInput = document.getElementById("repeat_signup_password");
 
-    if (formData.signup_password.value != formData.repeat_signup_password.value) {
-        repeatPasswordInput.setCustomValidity("Passwords does not match.");
-    } else {
+    if (validatePassword(formData.signup_password.value, formData.repeat_signup_password.value, repeatPasswordInput)) {
         repeatPasswordInput.setCustomValidity("");
         let res = serverstub.signUp({
             email: formData.signup_email.value,
@@ -63,20 +71,50 @@ function signup(formData) {
         });
         if (res.success) {
             handle_login(formData.signup_email.value, formData.signup_password.value);
-            document.getElementById("signup_form").reset();
         } else {
             communicateToUser(res.message);
         }
     }
-    repeatPasswordInput.reportValidity();
 }
 
-function communicateToUser(msg) {
-    let messageBoxText = document.getElementById("message_box_text");
-    let messageBox = document.getElementById("message_box");
+function validatePassword(psw1, psw2, pswInput) {
+    // NOTE: pswInput need to have oninput setCustomValidity to empty string.
+    if (psw1 != psw2) {
+        pswInput.setCustomValidity("Passwords does not match.");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function communicateToUser(msg, msg_box_prefix) {
+    let messageBoxText = document.getElementById(msg_box_prefix + "_message_box_text");
+    let messageBox = document.getElementById(msg_box_prefix + "_message_box");
     messageBoxText.innerText = msg;
 
     // Animation from: https://codepen.io/mikeCky/pen/WWjLEq
     messageBox.classList.remove("hidden_animation");
     setTimeout(function () { messageBox.classList.add("hidden_animation"); }, 3000);
+}
+
+function signOut() {
+    serverstub.signOut(getToken());
+    localStorage.removeItem("token");
+    displayView(false);
+}
+
+function changePassword(formData) {
+    let psw = formData.change_password_input.value;
+    let repeatPsw = formData.repeat_change_password_input.value;
+    let repeatPasswordInput = document.getElementById("repeat_change_password_input");
+    let form = document.getElementById("change_password_form");
+
+    if (validatePassword(psw, repeatPsw, repeatPasswordInput)) {
+        let res = serverstub.changePassword(getToken(), formData.current_password_input.value, psw);
+        console.log(res);
+        if (res.success) {
+            form.reset();
+        }
+        communicateToUser(res.message, "account_panel");
+    }
 }
