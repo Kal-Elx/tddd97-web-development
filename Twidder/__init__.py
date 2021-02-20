994f4d2951d1060b067cb8541a67f75ddd07629c
Kalle Johansson <kallej1johansson@gmail.com>
2021-02-20 11:16:25 +0100

    lab 3 step 2: wsgi server

--- Twidder/__init__.py
+++ Twidder/__init__.py
@@ -2,3 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 import Twidder.views  # nopep8
+
+from gevent.pywsgi import WSGIServer  # nopep8
+
+if __name__ == 'Twidder':
+    print('running')
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
